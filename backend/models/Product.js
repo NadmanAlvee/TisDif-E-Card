bb35835b1b145fb462a2d0b5200b4f8020cad40b
Nadman Alvee <youremail@example.com>
2025-03-14 13:07:55 +0600

    refactor product model and header scripts; remove redundant productId field and reposition header.js script inclusion

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -5,10 +5,6 @@ const ProductSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	productId: {
-		type: String,
-		required: true,
-	},
 	price: {
 		type: Number,
 		required: true,
