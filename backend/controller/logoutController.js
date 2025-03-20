d7c1aca705cb55a2882577d8b54fc76100907878
Nadman Alvee <youremail@example.com>
2025-03-20 17:53:20 +0600

    refactor logout functionality and update CSS variables; enhance session destruction in logoutController, adjust text color variables in account, header, and productDetails styles

--- backend/controller/logoutController.js
+++ backend/controller/logoutController.js
@@ -1,6 +1,12 @@
 const logout = (req, res, next) => {
-	res.clearCookie(process.env.COOKIE_NAME);
-	res.redirect("/");
+	req.session.destroy((err) => {
+		if (err) {
+			console.error("Error destroying session:", err);
+			return next(err);
+		}
+		res.clearCookie(process.env.COOKIE_NAME);
+		res.redirect("/");
+	});
 };
 
 module.exports = logout;
