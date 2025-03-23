1475c9eb6ac221a5f9b8a96e4bdb8c1a19a0e65f
Nadman Alvee <youremail@example.com>
2025-03-24 00:09:14 +0600

    enhance header and search functionality; add search button and bar, improve layout and styles for better user experience

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,17 @@
 // Hamburgerr
 const hamburger = document.getElementById("hamburger");
 const navbar = document.getElementById("navbar");
+const searchBtn = document.querySelectorAll(".searchBtn");
+const searchBar = document.getElementById("searchBar");
+const Header = document.getElementById("Header");
+
+// Search
+searchBtn.forEach((e) => {
+	e.addEventListener("click", () => {
+		searchBar.classList.toggle("active");
+		Header.classList.toggle("searchMode");
+	});
+});
 
 hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
