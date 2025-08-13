b4128bf38ab53c6a6368fe4fa6970d5a3de80fd9
NadmanAlvee <nadmanalvee@gmail.com>
2025-08-13 14:40:42 +0600

    Refactor getSlides middleware: move try-catch block to encompass the entire function for better error handling

--- backend/middlewares/utils/findSlides.js
+++ backend/middlewares/utils/findSlides.js
@@ -2,17 +2,21 @@ const { resolve } = require("path");
 const fs = require("fs");
 
 const getSlides = (req, res, next) => {
-	const slidesDir = resolve(__dirname, "../../../public/images/slides");
-	let slides;
-	fs.readdir(slidesDir, (err, files) => {
-		if (err) {
-			console.log("Error reading slides directory:", err);
-			return next(); // Proceed without images
-		}
-		slides = files.map((file) => "/images/slides/" + file);
-		res.locals.slides = slides;
-		next();
-	});
+	try {
+		const slidesDir = resolve(__dirname, "../../../public/images/slides");
+		let slides;
+		fs.readdir(slidesDir, (err, files) => {
+			if (err) {
+				console.log("Error reading slides directory:", err);
+				return next(); // Proceed without images
+			}
+			slides = files.map((file) => "/images/slides/" + file);
+			res.locals.slides = slides;
+			next();
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = getSlides;
